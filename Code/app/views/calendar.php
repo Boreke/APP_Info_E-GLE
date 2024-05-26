cb55d794bf6d66652fa19a53494e5f7677eed266
Aylhiza <103447118+Aylhiza@users.noreply.github.com>
2024-05-26 03:16:00 +0200

    ajout d'affichage des événements dans le calendrier

--- Code/app/views/calendar.php
+++ Code/app/views/calendar.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href= "../app/public/css/calendar.css">
+<link rel="stylesheet" href="../app/public/css/calendar.css">
 <body>
 <?php
 require "../app/controllers/header.php";
@@ -14,6 +14,14 @@ $movieId = isset($_GET['id']) ? $_GET['id'] : 1;
 $query = "SELECT * FROM film WHERE id_film = ?";
 $movieDetails = $db->read($query, [$movieId]);
 
+$seanceQuery = "SELECT * FROM diffuser WHERE Film_id_film = ?";
+$seances = $db->read($seanceQuery, [$movieId]);
+
+$seanceData = [];
+foreach ($seances as $seance) {
+    $seanceData[date('Y-m-d', strtotime($seance->film_date))] = ['time' => date('H:i', strtotime($seance->film_date)), 'id' => $seance->idseance];
+}
+
 if ($movieDetails) {
     $movie = $movieDetails[0]; 
     echo "<div class='movie-layout'>";
@@ -21,12 +29,12 @@ if ($movieDetails) {
     echo "<img class='cinema-image' src='" .  htmlspecialchars($movie->image_file) . "' alt='Cinema Image'>";
     echo "</div>";
     echo "<div class='movie-info'>";
-    echo "<p class='movie-title'><strong></strong> " . htmlspecialchars($movie->titre) . "</p>";
+    echo "<p class='movie-title'>" . htmlspecialchars($movie->titre) . "</p>";
     echo "<div class='movie-genre-duration'>";
-    echo "<p><span class='movie-detail-label'><strong>Genre:</strong></span> " . htmlspecialchars($movie->genre) . "</p>";
-    echo "<p><span class='movie-detail-label'><strong>Durée:</strong></span> " . htmlspecialchars($movie->duree) . "</p>";
+    echo "<p><strong>Genre:</strong> " . htmlspecialchars($movie->genre) . "</p>";
+    echo "<p><strong>Durée:</strong> " . htmlspecialchars($movie->duree) . " minutes</p>";
     echo "</div>";
-    echo "<p><span class='movie-detail-label'><strong>Synopsis:</strong></span><br><br> " . htmlspecialchars($movie->synopsis) . "</p>";
+    echo "<p><strong>Synopsis:</strong><br>" . htmlspecialchars($movie->synopsis) . "</p>";
     echo "</div>";
     echo "</div>";
 } else {
@@ -34,43 +42,36 @@ if ($movieDetails) {
 }
 ?>
 
-
-<br><br><br>br><br><br>
- 
- 
-    <div class="center-top">
-
-    </div>
-    <div class="container">
-        <div class="calendar">
+<br><br><br><br><br><br>
+<div class="center-top"></div>
+<div class="container">
+    <div class="calendar">
         <header>
             <h3></h3>
-            
             <button id="prev"></button>
             <button id="next"></button>
-            
         </header>
         <section>
             <ul class="days">
-            <li>Sun</li>
-            <li>Mon</li>
-            <li>Tue</li>
-            <li>Wed</li>
-            <li>Thu</li>
-            <li>Fri</li>
-            <li>Sat</li>
+                <li>Sun</li>
+                <li>Mon</li>
+                <li>Tue</li>
+                <li>Wed</li>
+                <li>Thu</li>
+                <li>Fri</li>
+                <li>Sat</li>
             </ul>
             <ul class="dates"></ul>
         </section>
-        </div>
-        <div class="rectangle">
+    </div>
+    <div class="rectangle">
         <h1 id="rectangleTitle"></h1>
-        </div>
-        </div>
-        <script src="<?=ASSETS?>js/calendar.js" defer></script>
-        <div>
-        </div>
-    
-    </body>
-    <?= $this->view("footer")?>
-    </html>
+    </div>
+</div>
+<script>
+    const seanceData = <?= json_encode($seanceData); ?>;
+</script>
+<script src="<?=ASSETS?>js/calendar.js" defer></script>
+</body>
+<?= $this->view("footer")?>
+</html>
