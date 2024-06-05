7d2abbc3cf334460186aff23ab0a50ced9776c02
Guilhem <bouryguilhem@gmail.com>
2024-06-05 20:45:20 +0200

    debut forum 2

--- Code/app/controllers/forum.php
+++ Code/app/controllers/forum.php
@@ -1,17 +1,41 @@
 <?php
 
-Class About extends Controller
+Class Forum extends Controller
 {
 	function index()
 	{
 		unset($_SESSION['error_message']);
+        $post_model=$this->loadModel("post");
+        $data['commentaires']= $post_model->getCommentaireById(1);
 		$data['page_title'] = "Forum";
 
         $db = new Database();
-        $query = "SELECT * FROM post p JOIN commentaire c ON p.idpost = c.post_id_post";
+        $query = "SELECT * FROM post";
         $data['posts'] = $db->read($query);
-
+        
 		$this->view("forum",$data);
 	}
+
+    function displayPost(){
+        if (is_array($data['posts']) && count($data['posts']) > 0){
+            foreach ($data['posts'] as $post){
+                echo '<div class="PostItem">
+                        <div class="post">
+                            <h2>' . $post->titre . '</h2>
+                        </div>
+                        <div class="commentaire"> 
+                             <h2>' . $post->contenu . '</h3> 
+                        </div>
+                        <button class="editPostBtn" data-id="<?= $post->id ?>" data-question="<?= $faq->question ?>" data-answer="<?= $faq->answer ?>">Edit</button>
+                      </div>';
+            } 
+                
+           
+        }else{
+            echo '<h1>No Posts found.</h1>';
+        }
+            
+        
+    }
 
 }
