6ba13594ddccad1444147f11a5529d8274a70a17
Boreke <hippomartin03@gmail.com>
2024-06-05 11:08:49 +0200

    modif mentions legales et cgu sans ajout de catégories

--- Code/app/models/mentions_legales_content.php
+++ Code/app/models/mentions_legales_content.php
@@ -1,3 +1,21 @@
 <?php
 
-return NULL;
+return [
+    'page_title' => 'Your Page Title',
+    'editor' => [
+        'name' => 'Hugo O\'NEILL',
+        'address' => '6 avenue du général Balfourier, 75016',
+        'phone' => '+33 6 37 87 51 34',
+        'email' => 'honeill2000@gmail.com',
+    ],
+    'intellectual_property' => "L'ensemble du contenu du site E-GLE, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de E-GLE, à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs. Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de E-GLE. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.",
+    'data_protection' => "Le site E-GLE peut être amené à collecter des données personnelles. Conformément à la loi "Informatique et Libertés" du 6 janvier 1978 modifiée, vous disposez d'un droit d'accès, de rectification, de modification et de suppression des données vous concernant. Vous pouvez exercer ce droit en envoyant un courrier à l'adresse suivante : [Votre Adresse] ou par email à [Votre Adresse Email].",
+    'responsibility' => "Le site E-GLE contient des liens hypertextes vers d'autres sites. Cependant, E-GLE n'a pas la possibilité de vérifier le contenu des sites ainsi visités, et n'assumera en conséquence aucune responsabilité de ce fait. E-GLE met en œuvre tous les moyens dont il dispose pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de E-GLE, et signaler toutes modifications du site qu'il jugerait utile. E-GLE n'est en aucun cas responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.",
+    'applicable_law' => "Les présentes mentions légales sont soumises au droit français et tout litige ou contestation qui pourrait naître de l'interprétation ou de l'exécution de celles-ci sera de la compétence exclusive des tribunaux dont dépend le siège de l'éditeur du site.",
+    'headers' => [
+        2 => '2. Propriété intellectuelle',
+        3 => '3. Protection des données personnelles',
+        4 => '4. Responsabilité',
+        5 => '5. Droit applicable'
+    ]
+];
